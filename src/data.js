d4bd8e98c81892ecfd32585e174ea1f7e1c827de
AnaisGonzalez <anaisg2.0@gmail.com>
2019-12-12 17:47:52 -0300

    ultimos arreglos html dinamico

--- src/data.js
+++ src/data.js
@@ -5,11 +5,11 @@ const dataLol = Object.values(LoL.data); /* .entries llama a las [key:value] */
 
 /* Funcion Filtrar x Roles */
 export function filtroXrol(dato) {
-  const resultadoFiltro = dataLol.filter(elemento => (elemento.tags.includes(dato)));
+  const resultadoFiltro = dataLol.filter((elemento) => (elemento.tags.includes(dato)));
   return resultadoFiltro;
 }
 /* ´Funcion filtrar x rol BOTON LUCHADORES´ */
 export function check(attack) {
-  const resultadoAttack = dataLol.filter(propiedad => (propiedad.info.includes(attack)));
+  const resultadoAttack = dataLol.filter((propiedad) => (propiedad.info.includes(attack)));
   return resultadoAttack;
-}
+} 
